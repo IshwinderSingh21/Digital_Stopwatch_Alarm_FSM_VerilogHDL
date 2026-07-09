a36f5e8adfef5061ca6b04463e8374ba2b83f79b
IshwinderSingh21 <ishusingh12346@gmail.com>
2026-07-10 02:19:05 +0530

    Update synchronizer.v

--- src/synchronizer.v
+++ src/synchronizer.v
@@ -2,19 +2,24 @@ module synchronizer(
     input wire asyn_in,
     input wire rst,
     input wire master_clk,
-    output reg sync_out
+    output wire sync_out
 );
     reg q1;
+    reg q2;
+    
     always @(posedge master_clk)
     begin
         if (rst) begin
             q1<=0;
-            sync_out<=0;
+            q2<=0;
         end
         else begin
             q1<=asyn_in;
-            sync_out<=q1;
+            q2<=q1;
         end
     end
+
+    assign sync_out = q2;
+    
 endmodule
 
